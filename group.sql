11b30c701a7c7ebe83bdba1ea55397ebacdd0223
ΣΤΥΛΙΑΝΟΣ ΚΟΥΙΜΑΝΗΣ <56820617+stelkoui@users.noreply.github.com>
2020-01-08 18:25:09 +0200

    Add files via upload

--- group.sql
+++ group.sql
@@ -8,16 +8,12 @@ pcolor varchar(25) not null,
 psize varchar (25) not null,
 pdescr varchar(45) not null,
 pquant int unsigned not null,
-pprice decimal(7,2) unsigned default 0,
-pcreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-pupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+pprice decimal(7,2) unsigned default 0
 );
 
 insert into product values
-(1,"adidas","superstar","white","44","leather",40,99, now(), now());
+(2,"adidas","superstar","white","44","leather",40,99);
 select * from product;
-
-update product set pprice =101 where pcode =1; 
 
  create table user(
   uid int auto_increment primary key,
@@ -26,9 +22,7 @@ update product set pprice =101 where pcode =1;
   fname varchar(20),
   lname varchar(20),
   email varchar(20),
-  unique(username),
-  ucreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  uupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  unique(username)
   );
  insert into user (username,password,fname,lname,email)
   values('admin','$2a$10$PHROMJqyTB3GrbJllGfMG.SLu9apzPFIYTS.DNepEzCwoLxgk4X26','Stelios','Kouimanis','stelios@mail.com');
@@ -55,32 +49,3 @@ select * from user_role;
 select * from user;
  delete from user where uid=5;
  
- 
- 
-## INVENTORY TABLE ## 
- create table inventory(
-icode int unsigned primary key auto_increment,
-iquant int unsigned default 0,
-pcode int unsigned null,
-constraint fk_inventory foreign key (pcode)
-references product (pcode) 
-);
-
-
-
-## ORDERS TABLE ##
-create table orders (
-ocode int unsigned primary key auto_increment,
-oquant int unsigned default 0,
-ototal decimal(7,2) unsigned default 0, 
-ucode int unsigned null,
-pcode int unsigned null,
-ocreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-oupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-constraint fk_orders1 foreign key (ucode)
-references user (ucode),
-constraint fk_orders2 foreign key (pcode)
-references product (pcode) 
-);
-
-
